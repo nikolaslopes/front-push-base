06c1af0ab91f38a659eae9aff74dad6a564959ee
nikolaslopes <nikolaslopes.dev@gmail.com>
2022-02-15 13:38:25 -0400

    :fire:

--- 3-JS-ES6+/01-variables/js/app.js
+++ 3-JS-ES6+/01-variables/js/app.js
@@ -1,2 +1,13 @@
+let consoles = ['Playstation', 'XBOX', 'Switch', 'Xbox series'];
 
-console.log('DOM');
+let games = ['The Last of Us', 'Hallo', 'Zelda', 'Infinite'];
+
+const availableGames = consoles.map((item, index) => {
+  return (`${index} ${item}: ${games[index]}`);
+});
+
+console.log("\n\n");
+
+console.log(availableGames);
+
+availableGames.forEach((item, index, array) => console.log(item));
