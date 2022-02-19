f28740f18f38193da9f63233d368bb3c175560f4
nikolaslopes <nikolaslopes.dev@gmail.com>
2022-02-19 09:51:05 -0400

    fire: creating elements using only js

--- 3-JS-ES6+/01-variables/js/app.js
+++ 3-JS-ES6+/01-variables/js/app.js
@@ -1,49 +1,28 @@
 let link = document.createElement('a');
-
 link.setAttribute('href', 'https://frontpush.com.br');
-
-let content = document.createTextNode('Acessar o FrontPUSH');
-
-link.appendChild(content);
-
-let container = document.querySelector("#app");
-
-container.appendChild(link);
-
 link.style.textDecoration = 'none';
 link.style.color = '#633BBC';
 link.style.fontSize = '32px';
 link.style.fontWeight = 'bold';
 
+let content = document.createTextNode('Acessar o FrontPUSH');
+link.appendChild(content);
+
+let container = document.querySelector("#app");
+container.appendChild(link);
+
 let box = document.querySelector(".box");
-
 box.appendChild(link);
-
 box.style.width = '100%';
 box.style.height = '30%';
 box.style.border = '2px solid white';
 box.style.borderRadius = '10px';
 box.style.padding = '42px';
-
 box.style.display = 'flex';
 box.style.justifyContent = 'center';
 box.style.alignItems = 'center';
 
-
-let list = ['a', 'b', 'c'];
-
-const mapList = list.map((item, index, array) =>  {
-  return item;
-});
-
-console.log(mapList);
-
-console.log("\n\n")
-
-list.forEach((item, index) => console.log(index));
-
 const media = window.matchMedia('(max-width: 540px)');
-
 if(media.matches) {
   box.style.borderColor = '#99C2FD';
   link.style.fontSize = '26px';
@@ -52,4 +31,12 @@ if(media.matches) {
   box.style.borderWidth = '3px';
 }
 
-// const button = document.querySelector
+let button = document.createElement('button');
+
+let buttonContent = document.createTextNode('Click me');
+button.appendChild(buttonContent);
+
+let buttonBox = document.querySelector('#button-box');
+buttonBox.appendChild(button);
+buttonBox.style.padding = '22px';
+
