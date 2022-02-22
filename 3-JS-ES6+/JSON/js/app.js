9b6d5f8589eb49de94bc0a7d0e143ef0934c3843
nikolaslopes <nikolaslopes.dev@gmail.com>
2022-02-22 19:30:24 -0400

    :fire: json

---
+++ 3-JS-ES6+/JSON/js/app.js
@@ -0,0 +1,8 @@
+
+const object = JSON.parse('{"id": 2, "name": "Xbox Series X", "Category": "Games" ,"amount": 2, "promotion": true}')
+console.log(object);
+
+console.log("\n\n");
+
+const json = JSON.stringify(object);
+console.log(json);
