206c80f6cd86057d6fdb03b5644baa927a4dfc2b
nikolaslopes <nikolaslopes.dev@gmail.com>
2022-03-10 00:49:36 -0400

    :fire:

--- 3-JS-ES6+/JS-ASYNC/js/app.js
+++ 3-JS-ES6+/JS-ASYNC/js/app.js
@@ -1,12 +1,28 @@
-var xhr = new XMLHttpRequest();
-// instanciando uma classe em uma variável que herda os métodos, e propriedades da classe
+var myPromise = function () {
+  return new Promise(function (resolve, reject) {
+    var xhr = new XMLHttpRequest();
+    // instanciando uma classe em uma variável que herda os métodos, e propriedades da classe
 
-xhr.open("GET", "https://api.github.com/users/nikolaslopes");
-xhr.send(null);
+    xhr.open("GET", "https://api.gub.com/users/nikolaslopes");
+    xhr.send(null);
 
-xhr.onreadystatechange = function () {
-  if (xhr.readyState === 4) {
-    console.log(JSON.parse(xhr.responseText));
-    console.log(xhr.readyState);
-  }
+    xhr.onreadystatechange = function () {
+      if (xhr.readyState === 4) {
+        if (xhr.status === 200) {
+          resolve(JSON.parse(xhr.responseText));
+          console.log(`The request code is ${xhr.status}`);
+        } else {
+          reject(`The request failed`);
+        }
+      }
+    };
+  });
 };
+
+myPromise()
+  .then(function (response) {
+    console.log(response);
+  })
+  .catch(function (error) {
+    console.log(error);
+  });
