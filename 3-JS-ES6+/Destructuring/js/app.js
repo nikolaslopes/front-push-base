3f022d5876e3791b4f6cc71a86f1baa05512602b
nikolaslopes <nikolaslopes.dev@gmail.com>
2022-03-13 11:03:15 -0400

    :fire:

--- 3-JS-ES6+/Destructuring/js/app.js
+++ 3-JS-ES6+/Destructuring/js/app.js
@@ -3,3 +3,9 @@ document.documentElement.addEventListener("mousemove", onMouseMove);
 function onMouseMove({ clientX, clientY }) {
   console.log(clientX, clientY);
 }
+
+const games = ["Tomb Raider", "Sackboy", "GodOfWar"];
+
+const [game1, game2, game3] = games;
+
+console.log(game1);
