2d76402655502d0cdab2a3774fd491bb5fd387a2
nikolaslopes <nikolaslopes.dev@gmail.com>
2022-03-15 00:45:42 -0400

    :fire:

--- 3-JS-ES6+/TODO/js/app.js
+++ 3-JS-ES6+/TODO/js/app.js
@@ -1,3 +1,17 @@
 let listElement = document.querySelector('.list');
-
 let inputElement = document.querySelector('input');
+let buttonElement = document.querySelector('button');
+
+let array = ['Fazer um café', 'Ver aulas', 'Jogar'];
+
+function renderTodo() {
+  array.forEach((item, index, array) => {
+    let todoElement = document.createElement('li');
+    let todoText = document.createTextNode(item);
+
+    todoElement.appendChild(todoText);
+    listElement.appendChild(todoElement);
+  });
+}
+
+renderTodo();
